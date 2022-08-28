3c8da051594a30d0d0d3e4894a2f2af8b9a7d68b
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-08-28 21:08:39 +0900

    initial

---
+++ pages/learning.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+import { Layout } from '../components/Layout'
+
+const learning = () => {
+  return (
+    <Layout title='learning'>
+      aaa
+    </Layout>
+  )
+}
+
+export default learning
