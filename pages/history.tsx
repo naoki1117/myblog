bbbc33802e75830c26ab9599ccecae3ad62a05a9
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-09-04 21:50:30 +0900

    sdfgh

--- pages/history.tsx
+++ pages/history.tsx
@@ -7,13 +7,13 @@ const contacts:NextPage = () => {
   return (
     <Layout title='history'>
       <Component>
-        <h1 className='underline underline-offset-4'>VBA</h1>
+        <h1 className='underline underline-offset-1'>VBA</h1>
         <p>vbaによる社内システム、ツールの設計とリリースを行う</p>
         <br/>
-        <h1 className='underline underline-offset-4'>HTML,CSS</h1>
+        <h1 className='underline underline-offset-1'>HTML,CSS</h1>
         <p>HTML,CSSを用いてポートフォリオサイトの作成</p>
         <br/>
-        <h1 className='underline underline-offset-4'>javascript(React,next.js)</h1>
+        <h1 className='underline underline-offset-1'>javascript(React,next.js)</h1>
         <p>Reactやnext.jsを用いたSPAサイト及びアプリの構築</p>
       </Component>
     </Layout>
